037baf3591ca2a225318a6966c698fa18b0c3197
Lawal Abubakar Babatunde <utility4all@gmail.com>
2025-03-05 13:23:19 +0100

    Feat: implemented fixes to protect against multiple distribution.

--- test/ERC1155TokenGatedDistributor.t.sol
+++ test/ERC1155TokenGatedDistributor.t.sol
@@ -162,4 +162,115 @@ contract ERC1155TokenGatedDistributorTest is Test {
 
         assertEq(rewardToken.balanceOf(user1), amount * BASE_FEE);
     }
+
+    function test_BurnTokensAfterDistribution() public {
+        // Setup initial state
+        vm.startPrank(owner);
+        rewardToken.mint(address(distributor), 100 * BASE_FEE);
+        distributor.mint(user1, PROOF_OF_ENGAGEMENT, 5);
+        distributor.mint(user2, PROOF_OF_ENGAGEMENT, 3);
+        vm.stopPrank();
+
+        // Record initial balances
+        uint256 user1InitialBalance = distributor.balanceOf(user1, PROOF_OF_ENGAGEMENT);
+        uint256 user2InitialBalance = distributor.balanceOf(user2, PROOF_OF_ENGAGEMENT);
+        
+        // Distribute tokens
+        vm.prank(owner);
+        distributor.distributeTokens();
+
+        // Verify tokens were burned
+        assertEq(distributor.balanceOf(user1, PROOF_OF_ENGAGEMENT), 0, "User1's tokens should be burned");
+        assertEq(distributor.balanceOf(user2, PROOF_OF_ENGAGEMENT), 0, "User2's tokens should be burned");
+        
+        // Verify holder count is updated
+        assertEq(distributor.getHolderCount(), 0, "All holders should be removed after distribution");
+    }
+
+    function test_PartialBurnOnDistribution() public {
+        // Setup: Mint more tokens than will be distributed
+        vm.startPrank(owner);
+        rewardToken.mint(address(distributor), 5 * BASE_FEE); // Only enough for 5 tokens
+        distributor.mint(user1, PROOF_OF_ENGAGEMENT, 10); // Mint 10 tokens
+        vm.stopPrank();
+
+        // Record initial balance
+        uint256 initialBalance = distributor.balanceOf(user1, PROOF_OF_ENGAGEMENT);
+        assertEq(initialBalance, 10, "Initial balance should be 10");
+
+        // Distribute tokens - should revert due to insufficient funds
+        vm.prank(owner);
+        vm.expectRevert("Insufficient funds");
+        distributor.distributeTokens();
+
+        // Verify no tokens were burned due to revert
+        assertEq(
+            distributor.balanceOf(user1, PROOF_OF_ENGAGEMENT),
+            initialBalance,
+            "Tokens should not be burned on failed distribution"
+        );
+    }
+
+    function test_MultipleBurnsInSingleDistribution() public {
+        // Setup multiple holders with different amounts
+        vm.startPrank(owner);
+        rewardToken.mint(address(distributor), 1000 * BASE_FEE);
+        distributor.mint(user1, PROOF_OF_ENGAGEMENT, 5);
+        distributor.mint(user2, PROOF_OF_ENGAGEMENT, 3);
+        distributor.mint(user3, PROOF_OF_ENGAGEMENT, 7);
+        vm.stopPrank();
+
+        // Record initial state
+        uint256[] memory initialBalances = new uint256[](3);
+        initialBalances[0] = distributor.balanceOf(user1, PROOF_OF_ENGAGEMENT);
+        initialBalances[1] = distributor.balanceOf(user2, PROOF_OF_ENGAGEMENT);
+        initialBalances[2] = distributor.balanceOf(user3, PROOF_OF_ENGAGEMENT);
+        
+        // Verify initial balances
+        assertEq(initialBalances[0], 5, "User1 initial balance incorrect");
+        assertEq(initialBalances[1], 3, "User2 initial balance incorrect");
+        assertEq(initialBalances[2], 7, "User3 initial balance incorrect");
+
+        // Distribute and burn tokens
+        vm.prank(owner);
+        distributor.distributeTokens();
+
+        // Verify all tokens were burned
+        assertEq(distributor.balanceOf(user1, PROOF_OF_ENGAGEMENT), 0, "User1's tokens not burned");
+        assertEq(distributor.balanceOf(user2, PROOF_OF_ENGAGEMENT), 0, "User2's tokens not burned");
+        assertEq(distributor.balanceOf(user3, PROOF_OF_ENGAGEMENT), 0, "User3's tokens not burned");
+
+        // Verify holder count is updated
+        assertEq(distributor.getHolderCount(), 0, "Holder count should be 0 after distribution");
+
+        // Verify reward token distribution
+        assertEq(rewardToken.balanceOf(user1), 5 * BASE_FEE, "User1 reward incorrect");
+        assertEq(rewardToken.balanceOf(user2), 3 * BASE_FEE, "User2 reward incorrect");
+        assertEq(rewardToken.balanceOf(user3), 7 * BASE_FEE, "User3 reward incorrect");
+    }
+
+    function test_HolderCountBeforeAndAfterDistribution() public {
+        // Setup initial holders
+        vm.startPrank(owner);
+        rewardToken.mint(address(distributor), 100 * BASE_FEE);
+        distributor.mint(user1, PROOF_OF_ENGAGEMENT, 2);
+        distributor.mint(user2, PROOF_OF_ENGAGEMENT, 3); 
+        distributor.mint(user3, PROOF_OF_ENGAGEMENT, 1);
+        vm.stopPrank();
+
+        // Verify initial holder count
+        assertEq(distributor.getHolderCount(), 3, "Initial holder count should be 3");
+
+        // Distribute tokens which burns the PROOF_OF_ENGAGEMENT tokens
+        vm.prank(owner);
+        distributor.distributeTokens();
+
+        // Verify holder count is 0 after distribution since all tokens were burned
+        assertEq(distributor.getHolderCount(), 0, "Holder count should be 0 after distribution");
+
+        // Verify individual balances are also 0
+        assertEq(distributor.balanceOf(user1, PROOF_OF_ENGAGEMENT), 0);
+        assertEq(distributor.balanceOf(user2, PROOF_OF_ENGAGEMENT), 0);
+        assertEq(distributor.balanceOf(user3, PROOF_OF_ENGAGEMENT), 0);
+    }
 } 
